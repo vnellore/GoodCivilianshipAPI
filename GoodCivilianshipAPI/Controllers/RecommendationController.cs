a765a423cdb30a96f89483db9fce54f936aacc53
Vamsi Nellore <vamsi.nellore@gmail.com>
2016-03-10 00:09:07 -0500

    Added new models and RecommendationController

---
+++ GoodCivilianshipAPI/Controllers/RecommendationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GoodCivilianshipAPI.Models;
+
+namespace GoodCivilianshipAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class RecommendationController : ApiController
+    {
+        [Route("recommendations")]
+        public IEnumerable<Recommendation> GetAllRecommendations()
+        {
+            List<Recommendation> recommendations = new List<Recommendation>();
+            Recommendation r1 = new Recommendation();
+            r1.Description = "Pot Hole in front of 30th Street";
+
+            Recommendation r2 = new Recommendation();
+            r2.Description = "Street Light out at JFK and Market St";
+
+            recommendations.Add(r1);
+            recommendations.Add(r2);
+
+            return recommendations;
+        }
+    }
+}
--- GoodCivilianshipAPI/Models/UserModels.cs
+++ GoodCivilianshipAPI/Models/UserModels.cs
@@ -22,6 +22,23 @@ namespace GoodCivilianshipAPI.Models
     /// <summary>
     /// 
     /// </summary>
+    public enum IncidentCategory
+    {
+        Report = 0,
+        Support = 1,
+        Learn = 2
+    }
+
+    public class Comment
+    {
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string User { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class GPSCoordinates
     {
         public int Latitude { get; set; }
@@ -34,13 +51,16 @@ namespace GoodCivilianshipAPI.Models
     public class Incident
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+        public IncidentCategory Category { get; set; }
         public string Description { get; set; }
         public GPSCoordinates GPSLocation { get; set; }
         public string LocationDescription { get; set; }
         public List<User> ReportedUsers { get; set; }
         public IncidentStatus Status { get; set; }
-        public List<string> Comments { get; set; }
+        public List<Comment> Comments { get; set; }
         public List<string> Images { get; set; }
+        public DateTime CreateTime { get; set; }
     }
 
     /// <summary>
@@ -58,4 +78,8 @@ namespace GoodCivilianshipAPI.Models
         public List<Incident> Incidents { get; set; }
     }
     
+    public class Recommendation
+    {
+        public string Description { get; set; }
+    }
 }
